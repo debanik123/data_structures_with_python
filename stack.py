88d223dffad9b225050ec3782a2b15e7f4e11c8f
Debanik Roy <41754802+debanik123@users.noreply.github.com>
2019-12-13 23:12:15 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+class stack:
+    def __init__(self):
+        self.items = []
+    def push(self,item):
+        self.items.append(item)
+    def pop(self):
+        self.items.pop()
+    def is_empty(self):
+        return self.items == []
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    def display(self):
+        return self.items
+    
+c = stack()
+c.push("a")
+c.push("b")
+#c.push("c")
+#c.push("d")
+print(c.display())
+print(c.peek())
+print(c.is_empty())
+print(c.display())
+
+c.pop()
+print(c.display())
+print(c.is_empty())
+
+c.pop()
+
+print(c.display())
+print(c.is_empty()) 
+
+
+# In[ ]:
+
+
+
+
